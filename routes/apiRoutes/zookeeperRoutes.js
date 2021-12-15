2c65fb6544c51ab4aa42c52eff7045984f050fd3
rclank <53981655+rclank@users.noreply.github.com>
2021-12-14 23:36:52 -0500

    finished

---
+++ routes/apiRoutes/zookeeperRoutes.js
@@ -0,0 +1,37 @@
+const { filterByQuery, findById, validateZookeeper, createNewZookeeper } = require('../../lib/zookeepers');
+const { zookeepers } = require('../../data/zookeepers.json');
+
+const router = require('express').Router();
+
+router.get('/zookeepers', (req, res) => {
+    let results = zookeepers;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+router.get('/zookeepers/:id', (req, res) => {
+    const result = findById(req.params.id, zookeepers);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+router.post('/zookeepers', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = zookeepers.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateZookeeper(req.body)) {
+        res.status(400).send('The zookeeper is not properly formatted.');
+    } else {
+        // add animal to json file and animals array in this function
+        const zookeeper = createNewZookeeper(req.body, zookeepers);
+        res.json(zookeeper);
+    }
+});
+
+module.exports = router;
